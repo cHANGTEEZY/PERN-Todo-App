4ad242795d7efd496dc16e59b523cbc2dcec112b
Sushank Gurung <sushankgurung@cHANGTEEZYs-M2-Air.local>
2024-07-22 21:50:27 +0545

    need to save user info to database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import InputTodo from "./components/InputTodo";
 import ListTodos from "./components/ListTodos";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
 
 function MainApp() {
   return (
@@ -21,6 +22,9 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}>
+            {" "}
+          </Route>
           <Route path="/" element={<MainApp />}></Route>
         </Routes>
       </Router>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,29 +1,37 @@
-import { Form } from "react-bootstrap";
 import "./Login.css";
-
+import { Link } from "react-router-dom";
+import Signup from "./Signup";
 const Login = () => {
   return (
     <>
-      <div className="login-container">
-        <form className="login-form">
-          <h1>Login</h1>
-          <div className="username-area">
-            <input type="uername" placeholder="Username" />
-          </div>
-          <div className="email-area">
-            <input type="email" placeholder="email" />
-          </div>
-          <div>
-            <label>
-              <input type="checkbox" />
-              Remember me
-            </label>
-          </div>
-          <div className="login-area">
-            <button className="login-button">Login</button>
-          </div>
-        </form>
-      </div>
+      <form className="login-container ">
+        <h1 className="login-header">Sign in</h1>
+        <div className="mb-4">
+          <label className="form-label ">Email </label>
+          <input
+            type="email"
+            className="form-control custom-input"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onFocus={(e) => (e.target.style.borderColor = "Black")}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control custom-input"
+            id="exampleInputPassword1"
+            onFocus={(e) => (e.target.style.borderColor = "Black")}
+          />
+        </div>
+        <div className="signup mt-2 d-flex justify-content-between align-items-center">
+          <button type="submit" className="btn btn-danger">
+            Login
+          </button>
+          <Link to="/signup">Signup</Link>
+        </div>
+      </form>
     </>
   );
 };
